daebdd02535af7d2d1e14c4e5f9ec94be621d67b
unknown <ashwini.senthilkumaran@kaviglobal.com>
2022-12-08 14:41:30 -0600

    added hompage single type

---
+++ Kavi-Website-Backend/kaviwebsitestrapi/src/api/optimization-page/services/optimization-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * optimization-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::optimization-page.optimization-page');
---
+++ Kavi-Website-Backend/kaviwebsitestrapi/src/api/right-menu/services/right-menu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * right-menu service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::right-menu.right-menu');
