c71fad19cde4603e01d3c462677a783f0ed05974
asdefkavi <105015711+asdefkavi@users.noreply.github.com>
2022-12-12 08:18:13 -0600

    Kaviwebsite backend (#6)
    * added hompage single type
    
    * code clean up
    
    * adding config
    
    * changing root folder name
    
    * clean up

---
+++ Kavi-Website-Backend/config/plugins.js
@@ -0,0 +1,28 @@
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'strapi-provider-upload-azure-storage',
+        providerOptions: {
+          account: env('STORAGE_ACCOUNT'),
+          accountKey: env('STORAGE_ACCOUNT_KEY'),
+          serviceBaseURL: env('STORAGE_URL'),
+          containerName: env('STORAGE_CONTAINER_NAME'),
+          cdnBaseURL: env('STORAGE_CDN_URL'),
+          defaultPath: 'assets',
+          maxConcurrent: 10
+        }
+      }
+    },
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: true,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    }
+  });
---
+++ Kavi-Website-Backend/src/api/right-menu/controllers/right-menu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * right-menu controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::right-menu.right-menu');
