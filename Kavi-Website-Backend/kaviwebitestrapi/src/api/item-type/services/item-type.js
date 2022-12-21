9d5f9b9310506f6cae93106c0f2ef988714ae87a
asdefkavi <105015711+asdefkavi@users.noreply.github.com>
2022-12-21 15:47:07 -0600

    Kaviwebsite backend (#12)
    * added hompage single type
    
    * code clean up
    
    * adding config
    
    * changing root folder name
    
    * clean up
    
    * updated settings.json
    
    * updated homepage components
    
    * updated components and homepage
    
    * updated homepage and components
    
    * added data to homepage

--- Kavi-Website-Backend/kaviwebitestrapi/src/api/item-type/services/item-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * item-type service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::item-type.item-type');
--- Kavi-Website-Backend/kaviwebitestrapi/src/api/left-menu/controllers/left-menu.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * left-menu controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::left-menu.left-menu');
