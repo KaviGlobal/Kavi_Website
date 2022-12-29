9e39add4ee47ec29037866e6855b1cb17984a51a
akhil.gorjilli-saven <akhil.gorjilli@saven.in>
2022-12-29 22:03:10 +0530

    <!--rotaion card changes done-->

--- Kavi-Website-Frontend/src/app/body-content/body-content.component.ts
+++ Kavi-Website-Frontend/src/app/body-content/body-content.component.ts
@@ -3,13 +3,25 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
 import { BodyContentService } from '../services/body-content.service';
 import { NgxTinySliderSettingsInterface } from 'ngx-tiny-slider';
 // import { MouseEvent } from '@agm/core';
-
+import { trigger, state, style, transition, animate } from '@angular/animations';
 declare const google: any;
 
 @Component({
   selector: 'app-body-content',
   templateUrl: './body-content.component.html',
-  styleUrls: ['./body-content.component.scss']
+  styleUrls: ['./body-content.component.scss'],
+  animations: [
+    trigger('flipState', [
+      state('active', style({
+        transform: 'rotateY(179deg)'
+      })),
+      state('inactive', style({
+        transform: 'rotateY(0)'
+      })),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class BodyContentComponent implements OnInit {
   public searchCriteria!: FormGroup;
@@ -19,6 +31,8 @@ export class BodyContentComponent implements OnInit {
   lat: number = 51.673858;
   lng: number = 7.815982;
   clientImages: any = [];
+  ourCientImages: any = [];
+  flip: string = 'inactive';
   @ViewChild('googleMap') googleMapEle!: ElementRef;
   @ViewChild("customControl", { static: true }) customControl!: ElementRef;
 
@@ -31,17 +45,30 @@ export class BodyContentComponent implements OnInit {
 
   ngOnInit(): void {
     this.screenheight = window.innerHeight;
-    // this.tinySliderConfig = {
-    //   arrowKeys: true,
-    //   autoWidth: true,
-    //   gutter: 10,
-    //   controlsText: ["<", ">"],
-    //   waitForDom: false,
-    //   controlsContainer: this.customControl.nativeElement //custom controls
-    // };
-    // this.bodyContent.getImage().subscribe((data: any) => {
-
-    // })
+    this.ourCientImages.push({
+      title: "Strategy",
+      link: 'assets/strategy.png',
+      flip: 'inactive',
+      description: 'Don’t settle just for the “What”. Also, demand the Blueprint on “How”.'
+    });
+    this.ourCientImages.push({
+      title: "Consulting",
+      link: 'assets/consulting.png',
+      flip: 'inactive',
+      description: 'Focus on solutions & Outcomes, not the ongoing head count.'
+    });
+    this.ourCientImages.push({
+      title: "R&D",
+      link: 'assets/R&D.png',
+      flip: 'inactive',
+      description: 'Drive Innovation; Optimize Cost.'
+    });
+    this.ourCientImages.push({
+      title: "full_spectrum",
+      link: 'assets/full_spectrum.png',
+      flip: 'inactive',
+      description: 'Full-suite of Data Analytics capabilities.'
+    });
     let images = [];
     for (let index = 1; index < 5; index++) {
       // console.log(`assets/client_images/${index}.png`)
@@ -49,6 +76,14 @@ export class BodyContentComponent implements OnInit {
     }
     this.clientImages.push(images);
     console.log(this.clientImages);
+  }
+
+  toggleFlip(index: number) {
+    console.log(this.ourCientImages[index].flip)
+    this.ourCientImages[index].flip = (this.ourCientImages[index].flip == 'inactive') ? 'active' : 'inactive'
+    // this.flip = (this.flip == 'inactive') ? 'active' : 'inactive';
+    console.log(this.ourCientImages)
+
   }
 
   ngAfterViewInit(): void {
