3deb1e4cafb36bb3629f9c0de2ab31559352866e
unknown <ashwini.senthilkumaran@kaviglobal.com>
2023-01-12 16:05:00 -0600

    code clean up

--- Kavi-Website-Backend/kaviwebitestrapi/src/api/right-menu/controllers/right-menu.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * right-menu controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::right-menu.right-menu');
