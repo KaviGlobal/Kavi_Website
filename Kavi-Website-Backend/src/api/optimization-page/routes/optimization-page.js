32d094c350d30aaba153f7d2fed07a881c0c6eba
asdefkavi <105015711+asdefkavi@users.noreply.github.com>
2023-01-12 18:37:49 -0600

    Revert "Kaviwebsite backend (#25)" (#26)
    This reverts commit 85bd7a1a31c8dc4420fd20d18e39933bc2017a22.

---
+++ Kavi-Website-Backend/src/api/optimization-page/routes/optimization-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * optimization-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::optimization-page.optimization-page');
