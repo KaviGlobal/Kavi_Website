ac69c307b5d0477cbf49bd3de8a2c7567c0a5776
Ashwini Senthil Kumaran <ashwini.senthilkumaran@kaviglobal.com>
2023-03-20 14:59:48 -0500

    updated contact form and contact page components and collection type

---
+++ Kavi-Website-Backend/src/api/about-kavi/controllers/about-kavi.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * about-kavi controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::about-kavi.about-kavi');
