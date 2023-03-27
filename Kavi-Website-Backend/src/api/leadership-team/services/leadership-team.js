2212424bc33080622cd25499a6c629c0476db098
Ashwini Senthil Kumaran <ashwini.senthilkumaran@kaviglobal.com>
2023-03-27 11:26:27 -0500

    leadership-team collection added

---
+++ Kavi-Website-Backend/src/api/leadership-team/services/leadership-team.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * leadership-team service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::leadership-team.leadership-team');
