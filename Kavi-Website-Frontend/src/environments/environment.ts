fd1e28c1e1c619666039b9ebb27745971b6b815e
Sheeba-at-880111024226 <sheeba.hency@kaviglobal.com>
2023-08-22 13:50:03 -0500

    updates to environment

--- Kavi-Website-Frontend/src/environments/environment.ts
+++ Kavi-Website-Frontend/src/environments/environment.ts
@@ -4,11 +4,19 @@
 
 export const environment = {
   production: false,
+  //development
   apiDetails: {
     apiUrl: 'https://kavi-strapi-app.azurewebsites.net/api',
     apiImgUrl: 'https://kavistrapiappstorage.blob.core.windows.net/strapi-uploads/assets/',
     apiCustomUrl: 'https://kavi-custom-api.azurewebsites.net/api'
-  }
+  }  
+  //production
+/*    apiDetails: {
+      apiUrl: 'https://kavi-strapi-app-prod.azurewebsites.net/api',
+      apiImgUrl: 'https://kavistrapiappprodstorage.blob.core.windows.net/strapi-uploads/assets/',
+      apiCustomUrl: 'http://kavi-custom-api.azurewebsites.net/api'
+    }
+  */
 };
 
 /*
